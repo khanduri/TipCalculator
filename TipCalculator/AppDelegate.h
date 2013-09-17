dbef770ff0eb44b1d2b37892c7264c7d6b7fb260
PrashantKhanduri-local <prashant.khanduri@gmail.com>
2013-09-17 16:23:22 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Prashant Khanduri - Hearsay on 9/17/13.
+//  Copyright (c) 2013 KDeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
